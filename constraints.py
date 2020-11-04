5d701adfa98b1b5fad2269550caeca273aa44790
Jiachun Xiang <jcxiang1230@gmail.com>
2020-11-03 22:29:46 -0500

    Update constraints.py

--- constraints.py
+++ constraints.py
@@ -16,6 +16,7 @@ class Constraints:
             if not (val, value) in self.buffer_tuple:
                 self.buffer_tuple[(val, value)] = []
             self.buffer_tuple[(val, value)].append(t)
+        return
 
     def get_scope(self):
         return list(self.scope)
@@ -23,4 +24,28 @@ class Constraints:
     def check_buffer_tuples(self, val):
         return tuple(val) in self.buffer_tuple
 
-    def check_asmt(self):
+    def asmts(self):
+        n = 0
+        vals = []
+        for s in self.scope:
+            # needs to improve to check if it is not assigned
+            if not s:
+                n = n + 1
+                vals.append(s)
+        return n, vals
+
+    def check_tuple_vaild(self, t):
+        check = False
+        for x, val in enumerate(self, scope):
+            # need to improve
+            if val:
+                check = True
+        return check
+
+    def check_buffer(self, val, value):
+        check = False
+        if (val, value) in self.buffer_tuple:
+            for b in self.buffer_tuple[val, value]:
+                if self.check_tuple_vaild(b):
+                    check = True
+        return check
