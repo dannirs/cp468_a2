c780224686e51e5e6fb9ee2c5ab099e27defcea2
dannirs <33897109+dannirs@users.noreply.github.com>
2020-11-04 18:05:07 -0500

    Update main.py
    added input functions

--- main.py
+++ main.py
@@ -1,5 +1,52 @@
 def main():
-    # get input from text file
+    
+    
+# get input from text file
+def print_board(board):
+    for i in range(len(board)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - ")
+
+        for j in range(len(board[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(board[i][j])
+            else:
+                print(str(board[i][j]) + " ", end="")
+
+
+def find_empty(board):
+    for i in range(len(board)):  # row
+        for j in range(len(board[0])):  # col
+            num = int(board[i][j])
+            if num == 0:
+                return (i, j)  
+    return None
+
+
+board = []
+
+filepath = 'sudoku_input.txt'
+with open(filepath) as fp:
+    line = fp.readline()
+    while line:
+        values = line.split(' ')
+        values.pop()
+        board.append(values)
+        line = fp.readline()
+        
+# for x in board:
+#    print(*x)
+
+print_board(board)
+print()
+empty = find_empty(board)
+print("First empty:", empty)
+
+
+    
 
     # ...
 
