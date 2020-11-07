8d77e8e298de8bad298159a35369477464efa16d
Jiachun Xiang <jcxiang1230@gmail.com>
2020-11-06 22:49:03 -0500

    Update algos.py

--- algos.py
+++ algos.py
@@ -5,22 +5,23 @@ from Constraints import *
 
 from csp import *
 
+
 def forwardCheck(self, key, value):
-    #still need to fix this
+    # still need to fix this
     savedData = {}
-    savedData[key] = copy.copy(self.csp.values[key])
+    savedData[key] = copy.copy(self.CSP.values[key])
     self.unassigned[key] = False
 
-    self.csp.values[key] = [value]
-    for Xk in self.csp.getNeighbors(key):
+    self.CSP.values[key] = [value]
+    for Xk in self.CSP.getNeighbors(key):
         index = 0
-        domain = self.csp.values[Xk]
+        domain = self.CSP.values[Xk]
         copied = False
         for dValue in domain:
             if (dValue == value):
                 if not copied:
-                  savedData[Xk] = copy.copy(domain)
-                  copied = True
+                    savedData[Xk] = copy.copy(domain)
+                    copied = True
                 domain.pop(index)
             else:
                 index += 1
