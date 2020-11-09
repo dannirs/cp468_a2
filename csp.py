99ab1e31acb6040f43b5d8305d2ffe00c3760961
Jiachun Xiang <jcxiang1230@gmail.com>
2020-11-09 15:33:13 -0500

    Update csp.py

--- csp.py
+++ csp.py
@@ -228,6 +228,3 @@ def infer(assignment, inferences, csp, var, val):
                     return "Fail"
 
     return inferences
-
-
-#csp = CSP('003020600900305001001806400008102900700000008006708200002609500800203009005010300')
